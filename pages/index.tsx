374da67fe626ac68810ec2c4fad15e958a579c21
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2022-12-12 14:44:19 -0300

    span delete href

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <nav className="bg-gray-800 p-2 mt-0 fixed w-full z-10 top-0">
         <div className="container mx-auto flex flex-wrap items-center">
 		    <div className="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
-				<span className="text-white no-underline hover:text-white hover:no-underline" href="#">
+				<span className="text-white no-underline hover:text-white hover:no-underline">
 					<span className="text-2xl pl-2"><i className="em em-grinning"></i> Bienvenido a <a className='text-violet-500'>Weeout</a> <span className="animate-waving-hand">👋🏻</span></span>
 				</span>
             </div>
