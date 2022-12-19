284d9b32ea2b01cd1a5758deaf39bd7cb1e27ddb
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2022-12-19 20:26:06 -0300

    acomodacion de codigo xd

--- pages/mainEvent/mainEvent.tsx
+++ pages/mainEvent/mainEvent.tsx
@@ -1,104 +1,55 @@
 import MyComponent from '../map/index';
 import Events from '../../src/components/events';
-
+import Ilustration from '../../src/components/Ilustracion/Ilustracion';
 export default function MainEvent () {
     return(  
-    
-        <section className="bg-white border-b py-8">
-      <div className="container max-w-5xl mx-auto m-8">
-        <div className="w-full mb-4">
-          <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
-        </div>
-        <div className="flex flex-wrap">
-          <div className="w-5/6 sm:w-1/2 p-6">
-            <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-            Busca un evento
-            </h3>
-            <div className="text-gray-600 mb-8">
-            <Events/>
+      <article className="grid gap-2">
+      <main className="grid grid-cols-[1fr_minmax(0px,1280px)_1fr] gap-6 gap-y-8">
+        <section className="grid grid-cols-12 col-start-2 gap-4 lg:gap-6 gap-y-8 content-start">
+          <h1 className="col-span-10 text-3xl mt-12">Busca lo que necesites en la Lista de eventos disponibles: </h1>
+
+          <div className="col-span-12 h-[13rem] lg:h-[31rem] w-full object-cover lg:col-span-5 lg:row-span-2">
+          <Events/>
+          </div>
+          <p className="text-xl col-span-12 lg:col-span-7"><MyComponent/> </p>
+          <p className="text-xs xl:text-sm col-span-12 sm:col-span-7 lg:col-span-4"><Ilustration/></p>               
+        </section>
+      </main>
+    </article>
+    //     <section className="bg-white border-b py-8 grid gap-6">
+    //   <div className="rid grid-cols-[1fr_minmax(0px,1280px)_1fr] gap-6 gap-y-8 ">
+    //     <div className="grid grid-cols-12 col-start-2 gap-4 lg:gap-6 gap-y-8 content-start">
+    //       <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+    //     </div>
+    //     <div className="flex flex-wrap">
+    //       <div className="col-span-12 h-[13rem] lg:h-[31rem] w-full object-cover lg:col-span-5 lg:row-span-2">
+    //         <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+    //         Busca un evento
+    //         </h3>
+    //         <div className="text-gray-600 mb-8">
+    //         
             
-              <br />
-              <br />
-            </div>
-          </div>
-          <div className="w-full sm:w-1/2 p-6">
-            <svg className="w-full sm:h-64 mx-auto" viewBox="0 0 1177 598.5" xmlns="http://www.w3.org/2000/svg">
-              <title>travel booking</title>
-              <rect x="171.5" y="111.25" width="834" height="456" rx="20.42" fill="#f2f2f2" />
-              <path d="m172 133.75h268v434h-247.58a20.42 20.42 0 0 1 -20.42 -20.42v-413.58z" fill="#ff6347" />
-              <path transform="translate(-11.5 -150.75)" d="M1017,282.42V294H183V282.42A20.42,20.42,0,0,1,203.42,262H996.58A20.42,20.42,0,0,1,1017,282.42Z" fill="#3f3d56" />
-              <circle cx="193" cy="127.75" r="6" fill="#ff6347" />
-              <circle cx="208" cy="127.75" r="6" fill="#ff6347" />
-              <circle cx="223" cy="127.75" r="6" fill="#ff6347" />
-              <path transform="translate(-11.5 -150.75)" d="M387.5,490A66.5,66.5,0,1,1,321,423.5,66.47,66.47,0,0,1,387.5,490Z" fill="none" stroke="#f2f2f2" stroke-miterlimit="10" stroke-width="2" />
-              <path transform="translate(-11.5 -150.75)" d="M325.38,467.23l8.3,13,35.53,55.59a66.5,66.5,0,0,1-103.32-8.57l43.54-84.94.91,1.43" fill="none" stroke="#f2f2f2" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-              <path
-                transform="translate(-11.5 -150.75)"
-                d="M385.31,507a66.46,66.46,0,0,1-16.1,28.82l-35.53-55.59,15.69-24.78a.66.66,0,0,1,1.1,0C353.76,460.32,371,486,385.31,507Z"
-                fill="none"
-                stroke="#f2f2f2"
-                stroke-miterlimit="10"
-                stroke-width="2"
-              />
-              <path transform="translate(-11.5 -150.75)" d="M337.5,452.5a15,15,0,0,1-12.12,14.73l-15-23.51a15,15,0,0,1,27.16,8.78Z" fill="none" stroke="#f2f2f2" stroke-miterlimit="10" stroke-width="2" />
-              <path transform="translate(-11.5 -150.75)" d="m347.5 481.5" fill="none" stroke="#f2f2f2" stroke-miterlimit="10" stroke-width="2" />
-              <path transform="translate(-11.5 -150.75)" d="m333.5 480.5" fill="none" stroke="#f2f2f2" stroke-miterlimit="10" stroke-width="2" />
-              <rect x="604" y="260.14" width="347" height="11" rx="1.24" fill="#ff6347" opacity=".3" />
-              <rect x="604" y="392.07" width="347" height="11" rx="1.24" fill="#ff6347" opacity=".3" />
-              <rect x="878" y="279.75" width="73" height="25" rx="1.24" fill="#ff6347" />
-              <rect x="878" y="411.75" width="73" height="25" rx="1.24" fill="#ff6347" />
-              <path transform="translate(-11.5 -150.75)" d="m978.18 606.93l-1.73 2s-21.05 2-20.2 5.39 25.35-4.55 25.35-4.55z" fill="#ffc1c7" />
-              <path transform="translate(-11.5 -150.75)" d="m1016.3 605.22s-22.5 8-34.74 4.56l5.69 11.39s29.05-0.86 34.18-6-5.13-9.95-5.13-9.95z" fill="#ff6584" />
-              <path transform="translate(-11.5 -150.75)" d="m1016.3 605.22s-22.5 8-34.74 4.56l5.69 11.39s29.05-0.86 34.18-6-5.13-9.95-5.13-9.95z" opacity=".1" />
-              <path
-                transform="translate(-11.5 -150.75)"
-                d="M1048.24,614.05l8.54,10.25S1071,657.62,1040,661s-54.11-2.57-54.11-2.57-12.53-6-12.82-1.42-1.42,17.37-8.26,29.9l-6,13.67a8.84,8.84,0,0,0-2.27,7.41c.85,4.27-19.09,5.69-18.8,0,0,0,2.85-4.84,2.85-7.69s4.55-9.68,4.55-9.68l8.26-41s-.57-21.08,18.8-17.09,47-.86,47-.86l6.26-21.92Z"
-                fill="#575a89"
-              />
-              <path
-                transform="translate(-11.5 -150.75)"
-                d="M999.16,721.79a5.79,5.79,0,0,0,5.14,6l134.88,3.33a.41.41,0,0,0,.32-.11h0a.31.31,0,0,0,.11-.2l1.79-8.32a6.38,6.38,0,0,0,.13-1.44l-2.88-60.37a5.65,5.65,0,0,0-.84-2.8l-2-3.36a1.12,1.12,0,0,0-.25-.28,1,1,0,0,0-.61-.2l-127,1.89a5.8,5.8,0,0,0-5.71,5.53Z"
-                fill="#3f3d56"
-              />
-              <path transform="translate(-11.5 -150.75)" d="M1135.53,654.27l4,76.78h0l.11-.2,1.79-8.32a6.38,6.38,0,0,0,.13-1.44l-2.88-60.37a5.65,5.65,0,0,0-.84-2.8l-2-3.36A1.12,1.12,0,0,0,1135.53,654.27Z" opacity=".1" />
-              <path
-                transform="translate(-11.5 -150.75)"
-                d="m1016.2 613.3s26.06-9.79 40.58 11c0 0 3.7 32.18-11.11 33.6s-34.17 1.14-38.73-3.7-33.32-18.51-33.32-18.51-14.32-7.3-17.12 2.19-3.1 11.77-3.1 11.77-14.81 10.82-20.79 12.81c0 0-4 5.7-6.26 5.7s-16.23 3.13-14.81-7.41l23.07-21.07 23.06-22.5s6.55-9.68 23.36-4.56a346.13 346.13 0 0 0 33.89 8.26z"
-                fill="#575a89"
-              />
-              <path
-                transform="translate(-11.5 -150.75)"
-                d="M955.68,707.46s5.41,13.67.86,15.38-14.24.57-14.24.57-11.4-1.43-14.24-1.14-14.53-2-12.82-6.55,10.54-3.42,10.54-3.42l8-3.7s.86-2.85,2.85-1.71S944.29,712.3,955.68,707.46Z"
-                fill="#cbceda"
-              />
-              <path
-                transform="translate(-11.5 -150.75)"
-                d="m926.06 667s6 13.38-3.7 12.24-17.09-3.13-19.93-3.41-10.49-2.92-9.3-8.19a4.52 4.52 0 0 0 0.08 -1c0-0.71 1-2.09 8.65-1.88 0 0 5.69 0 8-4-0.03-0.01 8.51 10.81 16.2 6.24z"
-                fill="#cbceda"
-              />
-              <path transform="translate(-11.5 -150.75)" d="m990.71 553.1s20.5-12.81 37.59 0 28.48 71.2 28.48 71.2l-39.59-8.83s-1.7-6.83-4-8-2.57-6-2.57-6-12.24-3.7-11.39-17.94-8.52-30.43-8.52-30.43z" fill="#ff6584" />
-              <path transform="translate(-11.5 -150.75)" d="m1001.2 553.67h-10.54s-6.27 7.12-7.12 25.06l-3.42 14.27s-7.12 17.09-2.85 19.94 12.54 2.56 14.24-3.13 13.44-38.76 13.44-38.76z" fill="#ff6584" />
-              <path
-                transform="translate(-11.5 -150.75)"
-                d="M998.23,509.4A15.27,15.27,0,0,1,1014,512c5.47,4.88,6.57,12.85,8,20s4.17,15.21,11,18a28.35,28.35,0,0,1-9.19-.27l10.25,9.19-17.27-5.63c-5.42-1.77-11.11-3.56-16.72-2.56-9.3,1.65-15.78,10.4-24.71,13.47l1-4.85-6,0a11.75,11.75,0,0,0,3.43-4,3.27,3.27,0,0,0-2.29-1.2c-2.5-15.59,6.76-31,18.81-41.17,2.38-2,5-3.92,8.06-4.42s6.64.87,7.71,3.78"
-                fill="#3c354c"
-              />
-            </svg>
-          </div>
-        </div>
-        <div className="flex flex-wrap flex-col-reverse sm:flex-row">
-          <div className="w-full sm:w-1/2 p-6 mt-6">
-            <div className="align-middle">
-              <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-              Localizalo 
-              </h3>
-              <div className=' '>
-                    <MyComponent/>        
-                  </div>    
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    //           <br />
+    //           <br />
+    //         </div>
+    //       </div>
+    //       <div className="col-span-12 lg:col-span-7">
+    //         
+    //       </div>
+    //     </div>
+    //     <div className="text-xs xl:text-sm col-span-12 sm:col-span-7 lg:col-span-4">
+    //       <div className="w-full sm:w-1/2 p-6 mt-6">
+    //         <div className="align-middle">
+    //           <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+    //           Localizalo 
+    //           </h3>
+    //           <div className=' '>
+    //                        
+    //               </div>    
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
     )
 }
