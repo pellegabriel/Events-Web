9b98be34f3e1448c44625f1ef92d6d9d2d7bff43
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2022-12-20 18:28:29 -0300

    Revert "..."
    This reverts commit ba2e80dbe6ed2b63e65fcd6ed7a7e913d38124ee.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,49 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "weeout": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "weeout921a0308DefineAuthChallenge": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "weeout921a0308CreateAuthChallenge": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "weeout921a0308VerifyAuthChallengeResponse": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "auth": {
+        "weeout921a0308": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "betatestGroupRole": "string"
+        }
+    },
+    "storage": {
+        "s3weeout01storage2ebbc512": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
