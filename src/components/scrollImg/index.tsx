f2772f516c09ed39d10f5bbeff2c2b62f8a3e99f
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2022-12-27 00:14:59 -0300

    ..

--- src/components/scrollImg/index.tsx
+++ src/components/scrollImg/index.tsx
@@ -5,6 +5,50 @@ interface IProps {
   events: Array<Event>
 }
 
+const fakeEvent = {
+  createdAt
+: 
+"2022-12-24T17:27:15.014Z",
+'endDate'
+: 
+"2023-12-16T04:56:00.000Z",
+id
+: 
+"657ea9f5-7234-42b6-945a-5d8dfa54ca33",
+is_done
+: 
+false,
+map_point
+: 
+"{ lat: -34.397, lng: 150.644 }",
+name
+: 
+"Gabriel Hernán Pelle",
+startDate
+: 
+"2023-06-09T16:20:00.000Z",
+types
+: 
+[],
+updatedAt
+: 
+"2022-12-24T17:27:15.014Z",
+user
+: 
+"        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, aperiam alias velit sapiente enim at ipsa aut quod adipisci sit laudantium ex culpa porro sint odio voluptas sed repudiandae error.",
+_deleted
+: 
+null,
+_lastChangedAt
+: 
+1671902835038,
+_version
+: 
+1
+}
+
+const fakeData = [fakeEvent, fakeEvent, fakeEvent, fakeEvent,fakeEvent, fakeEvent]
+
 export default function ScrollImg ({events}: IProps) {
     return (
     <main>
@@ -30,9 +74,11 @@ export default function ScrollImg ({events}: IProps) {
               id="scrollContainer"
               className="scrollbar flex flex-no-wrap overflow-x-scroll scrolling-touch items-start mb-8"
             >
-               {events.map((event) => (
-                <EventCard event={event} key={event.id} />
-              ))
+                 {fakeData.map((event, index) => {
+                console.log(event)
+                return (
+                <EventCard event={event} key={event.id + index}  />
+              )})
                 }  
            
             </div>
