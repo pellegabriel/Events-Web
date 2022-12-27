6bedd50628d087265c12cdd721faf565f902f1aa
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2022-12-27 00:14:59 -0300

    ..

--- src/components/eventCard/eventCard.tsx
+++ src/components/eventCard/eventCard.tsx
@@ -2,33 +2,42 @@ import Image from 'next/image'
 import { Event } from '../../models'
 import { useEffect, useState } from 'react'
 import { Storage } from 'aws-amplify';
+import img1 from '../../../public/IMG1.png' 
+
 
 interface IProps {
     event: Event
 }
 
-export default function EventCard ({event}: IProps) {
-    
-    const [image, setImage] = useState<string>()
+
+
+
+export default function EventCard ({event}: IProps | any) {
+    const [image, setImage] = useState<string>("")
+    const [error, setError] = useState<boolean>(false)
+
     const getUploadedImage = async () => {
         const file = await Storage.get(event.id, {
             level: "public"
         });
-        console.log({file, event})
         setImage(file)
     }
     useEffect(() => {
         getUploadedImage()
     }, [])
-    return (
 
+    const handleImageError = () => {
+        setError(true)
+    }
+
+    return (
                 <div
                 className="flex-none w-2/3 md:w-1/3 mr-8 md:pb-4 border rounded-lg"
                 >
                         <div className='border-b border-gray-300'  key={event.id}>
                         <div className="max-w-sm rounded-sm overflow-hidden shadow-lg m-3">
                             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                            {image && <Image alt='' src={image} width={100} height={100}/>}
+                            {error ? <Image alt='' src={img1} width={100} height={100}/> : <Image alt='' src={image} width={100} height={100} onError={handleImageError}/>}
                                 <div className="px-6 py-4">
                                     <div className="font-bold text-xl mb-2"> {event.name}</div>
                                     <div className="text-gray-700 text-base">
