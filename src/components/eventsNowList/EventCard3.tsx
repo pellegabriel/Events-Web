59e64a27195ce917f136be8fe54396289d4620fb
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2023-04-11 22:53:27 -0300

    EventCard3 update

--- src/components/eventsNowList/EventCard3.tsx
+++ src/components/eventsNowList/EventCard3.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import { Event } from '../../models'
 import { useEffect, useState } from 'react'
 import { Storage } from 'aws-amplify'
+import { useMemo } from 'react';
+
 
 interface IProps {
   event: Event
@@ -41,33 +43,106 @@ export default function EventCard2({ event }: IProps | any) {
     getUploadedAudio()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+  const [isHover, setIsHover] = useState(false)
 
+  const handleMouseEnter = () => {
+    setIsHover(true)
+  }
+  const handleMouseLeave = () => {
+    setIsHover(false)
+  }
+  const boxStyle = {
+    backgroundColor: isHover ? '#f43f5e' : '',
+    width: '200px',
+    padding: '8px',
+    color: 'white',
+    fontSize: '14px',
+    borderRadius: '6px',
+    border: '1px solid rgba(255, 255, 255, 0.6)',
+  }
+  const containerWidth = '60%';
+
+  const containerPixelWidth = useMemo(() => {
+    const parsedWidth = parseInt(containerWidth, 10);
+    if (!Number.isNaN(parsedWidth)) {
+      const parentWidth = document.querySelector('.parent')?.clientWidth ?? 0;
+      return (parentWidth * parsedWidth) / 100;
+    }
+    return undefined;
+  }, [containerWidth]);
   return (
     <div
-      className="flex bg-white rounded-lg mt-8 object-cover shadow-lg group-hover:opacity-75"
+      style={{
+        width: '1200px',
+        height: '300px',
+        display: 'flex',
+        padding: '24px',
+        overflow: 'auto',
+        borderRadius: '18px',
+        flexDirection: 'row',
+        marginBottom: '64px',
+        justifyContent: 'space-between',
+        border: '1px solid rgba(255, 255, 255, 0.6)',
+      }}
       key={event.id}
-      style={{ background: '#F4F6F6 ' }}
     >
-      {image && !error ? (
-        <Image
-          className=" p-4 "
-          alt=""
-          src={image}
-          width={400}
-          height={200}
-          onError={handleImageError}
-          style={{ width: '180px', height: '120px' }}
+      <div
+        style={{
+          width: '50%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div>
+          <h2
+            style={{ color: 'white', fontSize: '38px', marginBottom: '18px' }}
+          >
+          {event.name}
+          </h2>
+
+          <p style={{ color: 'white', fontSize: '14px', marginBottom: '14px' }}>
+            {event.descripcion}
+          </p>
+
+          <p style={{ color: 'white', fontSize: '14px', marginBottom: '14px' }}>
+            {event.startDate}
+          </p>
+
+          <p style={{ color: 'white', fontSize: '14px' }}>$ 2.000,00</p>
+        </div>
+
+        <button
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          style={boxStyle}
+        >
+          Ver más
+        </button>
+      </div>
+
+      <div
         />
-      ) : (
-        <Image
-          className=" p-4"
-          alt=""
-          src="/img1.jpg"
-          width={400}
-          height={200}
-          style={{ width: '180px', height: '120px' }}
-        />
-      )}
+        {image && !error ? (
+          <Image
+            className=" p-4 "
+            alt=""
+            src={image}
+            width={containerPixelWidth}            
+            height={200}
+            onError={handleImageError}
+            style={{ width: '180px', height: '120px' }}
+          />
+        ) : (
+          <Image
+            className=" p-4"
+            alt=""
+            src="/img1.jpg"
+            width={containerPixelWidth}            
+            height={200}
+            style={{ width: '180px', height: '120px' }}
+          />
+        )}
     </div>
   )
 }
--- src/components/eventsNowList/EventsNowList.tsx
+++ src/components/eventsNowList/EventsNowList.tsx
@@ -58,63 +58,29 @@ export default function EventsNowList({ events = [] }: IProps) {
     border: '1px solid rgba(255, 255, 255, 0.6)',
   }
   return (
-    <div
-      style={{
-        width: '1200px',
-        height: '300px',
-        display: 'flex',
-        padding: '24px',
-        overflow: 'auto',
-        borderRadius: '18px',
-        flexDirection: 'row',
-        marginBottom: '64px',
-        justifyContent: 'space-between',
-        border: '1px solid rgba(255, 255, 255, 0.6)',
-      }}
-    >
-      <div
-        style={{
-          width: '50%',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-        }}
-      >
-        <div>
-          <h2
-            style={{ color: 'white', fontSize: '38px', marginBottom: '18px' }}
-          >
-            Cerati en Club TRI
-          </h2>
-
-          <p style={{ color: 'white', fontSize: '14px', marginBottom: '14px' }}>
-            Club TRI • Mar del Plata, Provincia de Buenos Aires
-          </p>
-
-          <p style={{ color: 'white', fontSize: '14px', marginBottom: '14px' }}>
-            15 de abr. de 2023
-          </p>
-
-          <p style={{ color: 'white', fontSize: '14px' }}>$ 2.000,00</p>
-        </div>
-
-        <button
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          style={boxStyle}
-        >
-          Ver más
-        </button>
-      </div>
-
-      <div
-        style={{
-          width: '60%',
-          borderRadius: '14px',
-          backgroundImage:
-            'url(https://estudiantesdelaplata.com/wp-content/uploads/2015/09/CERATI.png)',
-        }}
-      />
-    </div>
-  )
+    <>
+    <EventCard3/>
+    {events.length === 0 ? (
+          <div className='flex '>
+          <Image
+                        alt=""
+                        src={svg7}
+                        width={300}
+                        height={300}
+                        className="mx-auto  dark:bg-gray-500"
+                        style={{marginLeft:'40px',}}
+                      />
+            <h1 className="  text-white text-xl font-bold leading-normal mb-1 flex justify-center items-center" style={{maxWidth:'500px'}}>Ups! No hay eventos sucediendo ahora.</h1>
+          </div>
+      ) : 
+      (
+        events.map((event: any) => {
+          return (
+            <Link href={`/events/${event.id}`} key={event.id}>
+              <EventCard3 event={event} key={event.id} />
+            </Link>
+          )
+        })
+      )}</>
+    )
 }
